b7531e8151eaa5e12b7fb6f4e4e45d7777f492b3
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2022-10-11 23:34:39 +0530

    Time: 2 ms (51.75%), Space: 6.5 MB (28.39%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:// going from star to robot
+    int uniquePaths(int n, int m) 
+    {
+        vector<vector<int>>dp(n,vector<int>(m));
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(i==n-1 || j==m-1)
+                    dp[i][j]=1;
+        for(int i=n-2;i>=0;i--)
+            for(int j=m-2;j>=0;j--)
+                dp[i][j]=dp[i][j+1]+dp[i+1][j];
+        return dp[0][0];
+        
+    }
+};
