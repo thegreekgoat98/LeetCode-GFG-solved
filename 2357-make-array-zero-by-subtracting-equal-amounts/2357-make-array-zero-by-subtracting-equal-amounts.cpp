022b2d1e924977ba1cc04312b5a2eaf48228a0ab
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2022-10-21 15:10:15 +0530

    Time: 7 ms (34.74%), Space: 8.5 MB (13.09%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) 
+    {
+
+        set<int>s(nums.begin(),nums.end());
+        return s.size()-s.count(0);
+    }
+    
+    //saw hint no 3
+};
