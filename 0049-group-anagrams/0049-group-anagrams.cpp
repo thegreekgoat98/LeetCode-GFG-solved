a67d3cce1733b2c5311744d8f73635ac03e2923b
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2022-10-28 19:20:38 +0530

    Time: 293 ms (6.82%), Space: 38.6 MB (5.03%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) 
+    {
+        vector<vector<string>>ans;
+        map<map<char,int>,vector<string>>mp;
+        
+        for(string str:strs)
+        {
+            map<char,int>fmap;
+            for(auto it:str)
+                fmap[it]++;
+            
+            auto x=mp.find(fmap);
+            
+            if(x==mp.end())
+            {
+                vector<string>temp;
+                temp.push_back(str);
+                mp[fmap]=temp;
+            }
+            else
+            {
+                x->second.push_back(str);
+            }
+        }
+        
+        for (auto it=mp.begin();it!=mp.end();it++)
+			ans.push_back(it->second);
+        
+        return ans;
+    }
+};
