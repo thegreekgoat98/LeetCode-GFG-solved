09cb26896882107eea3a82c6f1fc07952340914b
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2022-11-24 12:22:58 +0530

    Time: 297 ms (89.93%), Space: 8.1 MB (46.96%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,37 +1,37 @@
-class Solution {
+class Solution { //lc daily challange
 public:
     
-    bool dfs(int i,int j,vector<vector<char>>& board,string word,int k)
+    bool dfs(int i,int j,vector<vector<char>>& board,string& word,int k)
     {
         if(k==word.size())
             return true;
         if(i<0 || i==board.size() || j<0 || j==board[0].size() || board[i][j]!=word[k])
             return false;
+        
         char ch=board[i][j];
         board[i][j]='.';
         
-        bool a=dfs(i+1,j,board,word,k+1);
-        bool b=dfs(i-1,j,board,word,k+1);
-        bool c=dfs(i,j+1,board,word,k+1);
-        bool d=dfs(i,j-1,board,word,k+1);
+        int a=dfs(i+1,j,board,word,k+1);
+        int b=dfs(i-1,j,board,word,k+1);
+        int c=dfs(i,j+1,board,word,k+1);
+        int d=dfs(i,j-1,board,word,k+1);
+        
         board[i][j]=ch;
         
-        return a||b||c||d;
+        return a || b || c || d;
     }
-    ////////////
+    ////////////////////////////////
     bool exist(vector<vector<char>>& board, string word) 
     {
-        int n=board.size();
-        int m=board[0].size();
-        
-        for(int i=0;i<n;++i)
+        for(int i=0;i<board.size();++i)
         {
-            for(int j=0;j<m;++j)
+            for(int j=0;j<board[0].size();++j)
+            {
                 if(board[i][j]==word[0])
                     if(dfs(i,j,board,word,0))
                         return true;
+            }
         }
-        
         return false;
     }
 };
