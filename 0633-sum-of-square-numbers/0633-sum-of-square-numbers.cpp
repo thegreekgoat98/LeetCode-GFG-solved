eef8a4ac6262d47332eeecb509eb641e39a440e5
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2022-11-26 11:31:24 +0530

    Time: 6 ms (47.07%), Space: 6 MB (25.91%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) 
+    {
+        long l=0,r=sqrt(c);
+        while(l<=r)
+        {
+            long val=l*l+r*r;
+            if(val<c)
+                l++;
+            else if(val>c)
+                r--;
+            else
+                return true;
+        }
+        return false;
+    }
+};
