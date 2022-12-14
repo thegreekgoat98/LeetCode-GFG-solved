bbf5ec6fa962b50e7e5944a7c82bad28dd3b00b1
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2022-12-14 08:39:56 +0530

    Time: 2 ms (60.85%), Space: 7.9 MB (25.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,7 +5,7 @@ public:
         int n=arr.size();
         vector<int>dp(n); //in dp array, each index i represents, how much money can be robbed upto that index i.
         dp[0]=arr[0]; //upto first index, you only have one option that is the first index itself
-        if(n>=2)
+        if(n>=2) // if size of given array is 2 or more than 2
             dp[1]=max(arr[1],arr[0]);//upto second index, you have 2 options, either first index's or second index's itself, you will obviously choose maximum
         
         for(int i=2;i<n;++i) //now for each index, you can either choose either upto its last index, or the index itself and previous non adjacant element, i.e., i-2.
