d0126cae9afd02fd3dff1f51b456fe1d1596ec81
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2022-12-30 07:37:05 +0530

    Time: 21 ms (67.14%), Space: 12.1 MB (45.14%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void dfs(int curr,vector<int>adj[],vector<int>&path,vector<vector<int>>& graph)
+    
+    void dfs(int curr,vector<int>&path,vector<int>adj[],int sz)
     {
         path.push_back(curr);
-        if(curr==graph.size()-1)
+        if(curr==sz-1)
             ans.push_back(path);
         else
         {
             for(auto it:adj[curr])
-                dfs(it,adj,path,graph);
+                dfs(it,path,adj,sz);
         }
+        
         path.pop_back();
     }
-    ////////////
+    
+    
+    
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) 
     {
         int n=graph.size();
-        int i=0;
+        //building the adjacancy list
         vector<int>adj[n];
+        int i=0;
         for(auto it:graph)
         {
             vector<int>temp=it;
             for(auto jt:temp)
-            {
                 adj[i].push_back(jt);
-            }
             i++;
         }
-        ///////////////////////////
+        //dfs through node 0.
         vector<int>path;
-        dfs(0,adj,path,graph);
+        dfs(0,path,adj,n);
+        
         return ans;
     }
 };
