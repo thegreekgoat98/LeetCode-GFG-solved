1cc9d213e123034e02d0f49e3a7bcb1643078673
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-01-03 08:01:12 +0530

    Time: 67 ms (57.35%), Space: 12.1 MB (72.46%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -3,9 +3,9 @@ public:
     int minDeletionSize(vector<string>& strs) 
     {
         int cnt=0;
-        for(int i=0;i<strs[0].size();++i)
+        for(int i=0;i<strs[0].size();++i) //iterating through indices
         {
-            for(int j=1;j<strs.size();++j)
+            for(int j=1;j<strs.size();++j) //iterating through strings
             {
                 if(strs[j][i]<strs[j-1][i])
                 {
