cd92281e48fba0c76d0d96ca5502d8a9fc72941d
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-01-30 20:52:42 +0530

    Time: 4 ms (29.11%), Space: 6.8 MB (97.17%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string getStr(int first,int last)
+    {
+        return first==last ? to_string(first) : to_string(first) + "->" + to_string(last);
+    }
+    ////////////////////////////////////////////////////
+    vector<string> summaryRanges(vector<int>& nums) 
+    {
+        if(nums.size()==0)
+            return {};
+        
+        vector<string>ans;
+        int first=nums[0];
+        for(int i=1;i<nums.size();++i)
+        {
+            if(nums[i-1]+1!=nums[i])
+            {
+                ans.push_back(getStr(first,nums[i-1]));
+                first=nums[i];
+            }
+        }
+        ans.push_back(getStr(first,nums.back()));
+        return ans;
+    }
+};
