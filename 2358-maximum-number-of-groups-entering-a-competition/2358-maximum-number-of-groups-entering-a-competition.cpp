e3b4c9fe2fe419ee51fa47aa665e3d1c7cc0a7c5
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-02-02 17:58:45 +0530

    Time: 106 ms (65.17%), Space: 56.6 MB (14.75%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -6,5 +6,9 @@ public:
         return (-1+sqrt(1+8*n))/2;
     }
     
+//     used the formula--->          (-b+sqrt(b^2-4ac))/2a
+
+// formula for finding root of a quadratic equation
+    
     //https://www.youtube.com/watch?v=pNmLvLr9xek-- beautifully mathematically solved
 };
