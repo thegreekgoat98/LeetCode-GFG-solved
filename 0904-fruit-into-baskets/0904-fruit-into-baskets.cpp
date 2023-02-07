de9f3ed971081422f0643c6160336518d11ba2a7
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-02-07 14:51:30 +0530

    Time: 199 ms (47.87%), Space: 71.2 MB (73.34%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -4,22 +4,30 @@ public:
     int totalFruit(vector<int>& fruits) 
     {
         int n=fruits.size();
-        int k=2;
-        int ans=INT_MIN;
+        int ans=1;
         unordered_map<int,int>mp;
         int i=0,j=0;
         while(j<n)
         {
             mp[fruits[j]]++;
-            if(mp.size()>k)
+            if(mp.size()<2)
+            {
+                ans=max(ans,j-i+1);
+                j++;
+            }
+            else if(mp.size()==2)
+            {
+                ans=max(ans,j-i+1);
+                j++;
+            }
+            else if(mp.size()>2)
             {
                 mp[fruits[i]]--;
                 if(mp[fruits[i]]==0)
                     mp.erase(fruits[i]);
                 i++;
+                j++;
             }
-            ans=max(ans,j-i+1);
-            j++;
         }
         return ans;
     }
