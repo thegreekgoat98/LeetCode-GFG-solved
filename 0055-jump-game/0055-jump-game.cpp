c23e759b7a670053c5c82774c3c16fc7de0fb697
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-02-08 20:33:09 +0530

    Update 0055-jump-game.cpp

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,7 +4,7 @@ public:
     {
         // lets reverse the problem, you are at last index, can you reach the first index?
         int n=nums.size();
-        int ind=n-1;
+        int ind=n-1; //stores kahan pahunchna hai hame
         for(int i=n-1;i>=0;--i)
         {
             if(nums[i]+i>=ind)
