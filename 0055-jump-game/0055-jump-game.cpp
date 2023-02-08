e3447c734626a376983010b0fae8cfee49927586
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-02-08 20:30:31 +0530

    Time: 66 ms (77.46%), Space: 48.4 MB (47.91%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) 
     {
+        // lets reverse the problem, you are at last index, can you reach the first index?
         int n=nums.size();
         int ind=n-1;
-        for(int i=n-2;i>=0;--i)
+        for(int i=n-1;i>=0;--i)
         {
             if(nums[i]+i>=ind)
-            {
                 ind=i;
-            }
         }
+        
         return ind==0;
     }
     //https://www.youtube.com/watch?v=EgMPjWliYGY -- the best explanation
