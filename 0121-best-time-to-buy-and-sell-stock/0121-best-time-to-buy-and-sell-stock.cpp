5503ac500e28b23222c3c53762527777832e7420
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-02-25 15:04:09 +0530

    Time: 141 ms (61.66%), Space: 93.3 MB (56.82%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int ans=0;
+        int n=prices.size();
+        int i=0,j=0;
+        while(j<n)
+        {
+            if(prices[i]<prices[j])
+            {
+                int curr=prices[j]-prices[i];
+                ans=max(ans,curr);
+            }
+            else 
+                i=j;
+            
+            j++;
+        }
+        return ans;
+    }
+};
+// https://www.youtube.com/watch?v=1pkOgXD63yU --NEETCODE
