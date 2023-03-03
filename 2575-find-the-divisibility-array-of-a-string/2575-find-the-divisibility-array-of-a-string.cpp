1de458fe72cb5aa09b4b68c933a52ec07e1e24b3
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-03-03 20:44:28 +0530

    Time: 59 ms (40.84%), Space: 21.7 MB (50.82%) - LeetHub

---
+++ 2575-find-the-divisibility-array-of-a-string/2575-find-the-divisibility-array-of-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> divisibilityArray(string word, int m) 
+    {
+        vector<int>ans(word.size(),0);
+        long long num=0;
+        
+        for(int i=0;i<word.size();++i)
+        {
+            num=num*10+(word[i]-'0');
+            int rem=num%m;
+            num=rem;
+            if(rem==0)
+                ans[i]=1;
+        }
+        return ans;
+    }
+};
