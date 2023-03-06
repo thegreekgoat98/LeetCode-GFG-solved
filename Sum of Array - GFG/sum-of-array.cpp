ade93c418712b342fc3557760ab3086f3607c72b
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-03-06 20:32:35 +0530

    Added solution - LeetHub

---
+++ Sum of Array - GFG/sum-of-array.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// function to return sum of elements
+	// in an array of size n
+	
+	int sum(int arr[],int ind,int n)
+	{
+	    if(ind==n)
+	        return 0;
+	    
+	    return arr[ind]+sum(arr,ind+1,n);
+	}
+	
+	int sum(int arr[], int n)
+	{
+	    return sum(arr,0,n);
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.sum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
