efdd869497dfd459ec6fcc97f18d68f529133f54
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-04-03 14:51:32 +0530

    Time: 77 ms (93.80%), Space: 42 MB (11.29%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) 
+    {
+        sort(people.begin(),people.end());
+        
+        int cnt=0;
+        int i=0,j=people.size()-1;
+        while(i<=j)
+        {
+            if(people[i]+people[j]<=limit)
+            {
+                cnt++;
+                i++;
+                j--;
+            }
+            else
+            {
+                cnt++;
+                j--;
+            }
+        }
+        
+        return cnt;
+        
+    }
+};
