91b72b699720cb358690a4e43cc684dc01894888
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-04-14 07:21:36 +0530

    Time: 101 ms (77.74%), Space: 10.9 MB (71.41%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int LCS(string x,string y)
+    {
+        int n=x.length();
+        int m=y.length();
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++)
+            for(int j=0;j<=m;j++)
+                if(i==0||j==0)
+                    dp[i][j]=0;
+        //////
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=m;j++)
+            {
+                if(x[i-1]==y[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        return dp[n][m];
+    }
+    
+    int longestPalindromeSubseq(string y) 
+    {
+        string x=y;
+        reverse(y.begin(),y.end());
+        
+        return LCS(x,y);
+    }
+};
