31de84c1f177ad508a4304aac18d67867f786f1e
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-06-19 21:15:01 +0530

    Time: 6 ms (28.46%), Space: 8 MB (54.63%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) 
+    {
+        int ans=0;
+        int curr=0;
+        
+        for(int i=0;i<gain.size();++i)
+        {
+            curr+=gain[i];
+            ans=max(ans,curr);
+        }
+        
+        return ans;
+    }
+};
