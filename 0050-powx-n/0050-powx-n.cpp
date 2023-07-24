d25720df7d657563a244f5328edfd7bbb3522686
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-07-24 21:55:34 +0530

    Time: 4 ms (40.18%), Space: 6 MB (28.53%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    double solve(double x, long n)
+    {
+        if(n==0)
+            return 1;
+        
+        if(n<0)
+            return solve(1/x,-n);
+        
+        if(n%2==0)
+            return solve(x*x,n/2);
+        
+        return x*solve(x*x,(n-1)/2); //if 
+    }
+    
+    double myPow(double x, int n) 
+    {
+        return solve(x,(long)n);
+    }
+};
