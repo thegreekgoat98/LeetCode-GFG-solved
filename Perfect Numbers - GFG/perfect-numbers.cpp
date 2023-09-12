edac4414cb47b52170894c04350f6bda185f9e78
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2023-09-12 21:37:13 +0530

    Added solution - LeetHub

---
+++ Perfect Numbers - GFG/perfect-numbers.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int isPerfectNumber(long long N) 
+    {
+        unordered_set<long long>S={6,28,496,8128,33550336,8589869056,137438691328};
+        return S.count(N);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.isPerfectNumber(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
