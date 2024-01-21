6996ce0d3b473686e7426ab6fedaab225be2326f
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2024-01-21 19:49:10 +0530

    Time: 0 ms (100.00%), Space: 9 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     int n;
     int dp[101];
-    int solve(int currInd,vector<int>&nums)
+    int solve(vector<int>&arr,int currInd)
     {
         if(currInd>=n)
             return 0;
-        
         if(dp[currInd]!=-1)
             return dp[currInd];
-        int steal=nums[currInd]+solve(currInd+2,nums);
-        int not_steal=solve(currInd+1,nums);
+        int steal=arr[currInd]+solve(arr,currInd+2);
+        int not_steal=solve(arr,currInd+1);
         
         return dp[currInd]=max(steal,not_steal);
     }
-    /////////////////
+    /////////////////////////////
     int rob(vector<int>& nums) 
     {
         n=nums.size();
         memset(dp,-1,sizeof(dp));
-        return solve(0,nums);
-  
+        return solve(nums,0);
     }
 };
