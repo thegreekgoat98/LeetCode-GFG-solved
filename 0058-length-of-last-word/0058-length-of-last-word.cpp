905f64f738328dfb12e4be4953f3e78f973705b7
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2024-04-01 21:51:02 +0530

    Time: 4 ms (47.87%), Space: 7.7 MB (20.26%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -4,13 +4,12 @@ public:
     {
         int n=s.size();
         int cnt=0;
-        for(int i=n-1;i>=0;--i)
+        for(int i=n-1;i>=0;i--)
         {
-            if(s[i]==' ' && cnt>0)
-                return cnt;
             if(s[i]!=' ')
                 cnt++;
-            
+            if(s[i]==' ' and cnt>0)
+                return cnt;
         }
         return cnt;
     }
