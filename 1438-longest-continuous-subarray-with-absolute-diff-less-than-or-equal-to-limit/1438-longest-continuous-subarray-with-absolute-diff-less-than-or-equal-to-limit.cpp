69bea0db80ee81c8e2e5b0278b72116d7faeeb33
Chinmoy Ranjan Khadgaray <92818746+thegreekgoat98@users.noreply.github.com>
2024-06-23 17:22:08 +0530

    Time: 99 ms (56.79%), Space: 65.2 MB (43.37%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) 
     {
+        int n=nums.size();
         map<int,int>mp;
         int i=0,j=0;
-        int maxLen=INT_MIN;
-        while(j<nums.size())
+        int ans=INT_MIN;
+        while(j<n)
         {
             mp[nums[j]]++;
-            while((--mp.end())->first-mp.begin()->first>limit)
+            
+            while((--mp.end())->first - mp.begin()->first > limit)
             {
                 mp[nums[i]]--;
                 if(mp[nums[i]]==0)
                     mp.erase(nums[i]);
                 i++;
             }
+            ans=max(ans,j-i+1);
             j++;
-            maxLen=max(maxLen,j-i+1);
         }
-        return maxLen-1;
+        return ans;
     }
 };
+
+//https://leetcode.com/submissions/detail/844031276/ ----> saw my previous solution to this question. And understood
